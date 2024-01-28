d8c098a487f442c421ccaff35abdcabda6bb9819
Vladislav <vladuxexel@gmail.com>
2024-01-28 19:21:30 +0700

    Перенос отзывов в отдельный компонент

--- src/components/restaurant/component.tsx
+++ src/components/restaurant/component.tsx
@@ -1,19 +1,14 @@
 import { Restaurant as TRestaurant } from "@models";
-import { Review } from "../review/component";
 import styles from "./styles.module.scss"
 import { Menu } from "../menu/component";
+import { Rating } from "../rating/component";
 
 export const Restaurant = ({ restaurant }: { restaurant: TRestaurant }) => {
     return (
         <div className={styles.restaurant}>
             <h1>{restaurant.name}</h1>
             <Menu menu={restaurant.menu} />
-            <div className={styles.rating}>
-                <h3>Reviews:</h3>
-                <ul>
-                    {restaurant.reviews.map((review) => <li><Review review={review} /></li>)}
-                </ul>
-            </div>
+            <Rating reviews={restaurant.reviews} />
         </div>
     );
 }
